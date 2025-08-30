8cfdf5a1712dad259c41246df4975267ec2f394f
ranjanonjob <ranjanonjob@gmail.com>
2025-08-30 15:24:28 +0530

    coroutine details

--- p/py-25/py-core/lyn-ex-files/Ex_Files_Python_Advanced_2019/04 Collections/defaultdict_finished.py
+++ p/py-25/py-core/lyn-ex-files/Ex_Files_Python_Advanced_2019/04 Collections/defaultdict_finished.py
@@ -11,13 +11,12 @@ def main():
     # use a dictionary to count each element
     # fruitCounter = defaultdict(int)
     fruitCounter = defaultdict(lambda: 10)
-
     # Count the elements in the list
     for fruit in fruits:
         fruitCounter[fruit] += 1
 
     # print the result
-    for (k, v) in fruitCounter.items():
+    for k, v in fruitCounter.items():
         print(k + ": " + str(v))
 
 
